8bc778d9b4985c021c1c3239903c7ee907e95d1b
Sabarish370 <blackspidey370@gmail.com>
2025-11-10 20:43:29 +0530

    Fix image paths and update HeroPic component

--- src/components/aboutMeSection/AboutMain.jsx
+++ src/components/aboutMeSection/AboutMain.jsx
@@ -6,7 +6,7 @@ const AboutMain = () => {
   return (
     <div id='about' className='flex sm:flex-col md:flex-row gap-12 px-4 max-w-[1200px] mx-auto mt-100px justify-around items-center'>
         <AboutText />
-        {/* <AboutImage /> */}
+        <AboutImage />
     </div>
   )
 }
--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-      <img src="/images/experience-image.png" alt="my experience" />
+      <img src="src/assets/experience-image.png" alt="my experience" />
     </div>
   )
 }
